b60ef2b4fd46f6169c4ee913aff8854d6a5ebd55
Arth Akhouri <arthakhouri@gmail.com>
2024-07-08 12:06:09 +0100

    Modfied job files

--- test.sh
+++ test.sh
@@ -1,26 +1,26 @@
 #!/bin/sh
 # Grid Engine options (lines prefixed with #$)
-#$ -N First Run              
-#$ -cwd       
-#$ -o /outputs/
-#$ -e /errors/   
+#$ -N hello              
+#$ -cwd                  
 #$ -l h_rt=12:00:00  # Request a runtime
 #$ -q gpu          # Submit the job to the gpu queue
 #$ -pe gpu-a100 1  # Request NNODE A100 GPUs
 #$ -l h_vmem=16G    # Request memory per core
 #$ -l rl9=true     # Use rocky linux true
-
+#  These options are:
+#  job name: Test
+#  use the current working directory: -cwd
+#  runtime limit of 5 minutes: -l h_rt
+#  memory limit of 1 Gbyte: -l h_vmem
 # Initialise the environment modules
 . /etc/profile.d/modules.sh
-source /exports/applications/support/set_qlogin_environment.sh
 
 # Setting up the environment
 source /home/s2569758/miniconda3/bin/activate base
 
 # Load Python
 module load python/3.11.4
-module load cuda
-# pip install -r requirements.txt
+module load cuda/12.1.1
 
 # Run the program
 python script/run.py -c config/transductive/inference.yaml --dataset FB15k237 --epochs 500 --bpe null --gpus [0] --ckpt null
